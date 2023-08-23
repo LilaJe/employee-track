d1e712652d49d4ec3cc6151100e0b5cea595af05
Lila Ellis <lilaellis@lilas-mpb.lan>
2023-08-23 02:41:18 -0500

    finished product. Had another employee tracker but messed up and was accidentally committing everything to that one, so sorry for the zero commits.

--- index.js
+++ index.js
@@ -135,3 +135,77 @@ function updateRole() {
       );
     });
 }
+
+function viewRoles() {
+  db.query("SELECT * FROM role", function (err, results) {
+    console.table(results);
+    mainM();
+  });
+}
+
+function addRole() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "roleName",
+        message: "What is the name of the role?",
+      },
+      {
+        type: "input",
+        name: "salary",
+        message: "What is the salary for this role?",
+      },
+      {
+        type: "input",
+        name: "departmentId",
+        message: "What is the department ID for this role?",
+      },
+    ])
+    .then(function (answer) {
+      db.query(
+        "INSERT INTO role SET ?",
+        {
+          title: answer.roleName,
+          salary: answer.salary,
+          department_id: answer.departmentId,
+        },
+        function (err) {
+          if (err) throw err;
+          console.log("Role added successfully!");
+          mainM();
+        }
+      );
+    });
+}
+
+function viewDepartments() {
+  db.query("SELECT * FROM department", function (err, results) {
+    console.table(results);
+    mainM();
+  });
+}
+
+function addDepartment() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "departmentName",
+        message: "What is the name of the department?",
+      },
+    ])
+    .then(function (answer) {
+      db.query(
+        "INSERT INTO department SET ?",
+        {
+          name: answer.departmentName,
+        },
+        function (err) {
+          if (err) throw err;
+          console.log("Department added successfully!");
+          mainM();
+        }
+      );
+    });
+}
